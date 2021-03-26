942b3ef4d21a22f4f3fca09b86614a5b3b863054
NARAYANAREDDY37 <narayanareddychinnapureddy@yahoo.com>
2021-03-26 10:24:02 +0530

    fixed the bugs

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -10,13 +10,13 @@ function Cards() {
         <div className="cards__wrapper">
           <ul className="cards__items">
             <CardItem
-              src="../images/img-9.jpg"
+              src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/mata-atlantica-atlantic-forest-in-brazil-royalty-free-image-935746242-1567619461.jpg"
               text="Explore the hidden waterfall deep inside the Amazon Jungle"
               label="Adventure"
               path="/services"
             />
             <CardItem
-              src="../images/img-2.jpg"
+              src="https://a.cdn-hotels.com/gdcs/production75/d966/8994658f-13ad-4106-bde4-856450359f47.jpg"
               text="Travel through the Islands of Bali in a Private Cruise"
               label="Luxury"
               path="/services"
@@ -24,19 +24,19 @@ function Cards() {
           </ul>
           <ul className="cards__items">
             <CardItem
-              src="../images/img-1.jpg"
+              src="https://i.ytimg.com/vi/0_xGFCyVKN4/maxresdefault.jpg"
               text="Set Sail in the Atlantic Ocean visiting Uncharted Waters"
               label="Mystery"
               path="/services"
             />
             <CardItem
-              src="../images/img-4.jpg"
+              src="https://sumfinity.com/wp-content/uploads/2018/10/Misurina-Civetta-Mountain-football-pitch-Dolomites-Italy-daylight.jpg"
               text="Experience Football on Top of the Himilayan Mountains"
               label="Adventure"
               path="/products"
             />
             <CardItem
-              src="../images/img-8.jpg"
+              src="https://www.planetjanettravels.com/wp-content/uploads/2020/09/2_Morocco-Sahara-camel-ride.jpg"
               text="Ride through the Sahara Desert on a guided camel tour"
               label="Adrenaline"
               path="/sign-up"
